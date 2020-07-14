244ddd2b1b34c29423d6d0626e342c81cc214482
Artur Barseghyan <artur.barseghyan@target-holding.nl>
2020-07-14 12:13:46 +0200

    More

--- podman_install.sh
+++ podman_install.sh
@@ -7,7 +7,7 @@ dcr="$dc run --rm"
 # Thanks to https://unix.stackexchange.com/a/145654/108960
 log_file="sentry_install_log-`date +'%Y-%m-%d_%H-%M-%S'`.txt"
 exec &> >(tee -a "$log_file")
-
+# https://github.com/containers/podman/issues/6816#issuecomment-652739674
 setfacl -Rb ~/.local/share/containers
 
 MIN_RAM=2400 # MB
